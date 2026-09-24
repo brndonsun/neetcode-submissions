e4082a7b21a8c8239aaaf50a84b6a95b3658e829
brndonsun <150762321+brndonsun@users.noreply.github.com>
2026-09-24 12:50:04 -0700

    Add: meeting-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-0.py
@@ -0,0 +1,26 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        #intuition: sort and check for overlaps, minimum amount of rooms
+        # is the maximum number of overlaps are any point
+        intervals.sort(key=lambda x: x.start)
+        min_heap = []
+
+        for interval in intervals:
+            if min_heap and min_heap[0] <= interval.start:
+                heapq.heappop(min_heap)
+            heapq.heappush(min_heap, interval.end)
+
+        return len(min_heap)
+
+
+
+
+        # [40]
